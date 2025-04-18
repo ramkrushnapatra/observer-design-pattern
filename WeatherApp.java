4e686b409a9fd2298543ee221bd4b1cf7cebfae3
=ramkrushna <you@example.com>
2025-04-18 18:20:19 +0530

    observer  design pattern

---
+++ WeatherApp.java
@@ -0,0 +1,17 @@
+public class WeatherApp {
+    public static void main(String[] args) {
+        WeatherStation weatherStation = new WeatherStation();
+
+        Observer phoneDisplay = new PhoneDisplay();
+        Observer tvDisplay = new TVDisplay();
+
+        weatherStation.addObserver(phoneDisplay);
+        weatherStation.addObserver(tvDisplay);
+
+        // Simulating weather change
+        weatherStation.setWeather("Sunny");
+        weatherStation.notifyObservers();
+
+
+    }
+}
